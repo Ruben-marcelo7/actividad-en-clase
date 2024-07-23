3bc49b2f3a275c8f3d946331849af89ed34ec53e
Rubén <ruben.cobos@tecnologicoliceocristiano.edu.ec>
2024-07-22 21:30:35 -0500

    hola

---
+++ PromedioTresNumeros.py
@@ -0,0 +1,6 @@
+n1 = float(input("escriba el primer numero: "))
+n2 = float(input("escriba el segundo numero: "))
+n3 = float(input("escriba el tercer numero: "))
+S = n1 + n2 + n3 
+P = S/3
+input ("el promedio de los 3 numeros es igual: "+ str(P))
---
+++ no se.py
@@ -0,0 +1,7 @@
+valor = float(input("Ingrese valor a calcular iva: "))
+print("tuvalor es ", valor)
+conIva = valor * numpyfloat32(1.15)
+print("con iva es", conIva)
+iva = valor * 0.15 
+print("el iva es" , iva)
+print ("el porcentaje es", 1.15)
