0750e575a93c402102324c8f825fd46eabc2625f
Rubén <ruben.cobos@tecnologicoliceocristiano.edu.ec>
2024-07-23 21:07:56 -0500

    MensajePantalla

---
+++ MensajePantalla.py
@@ -0,0 +1,10 @@
+E = int(input("ingrese su edad: "))
+A = int(input("ingrese la cantidad de articulos: "))
+if E > 17:
+    print ("usted es mayor de edad ")
+else:
+    print ("usted no es mayor de edad ")
+if A >= 1:
+    print("usted adquirio 1 o mas articulos ")
+else:
+    print("usted no adquirio productos")
